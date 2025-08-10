1894761abc31bfdae23000bb50c192cdb8277131
JackyL100 <the.axew.27@gmail.com>
2025-08-09 20:07:52 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <webgpu/webgpu.h>
+int main() 
+{
+
+    // We create a descriptor
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
+
+    // We create the instance using this descriptor
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    // We can check whether there is actually an instance created
+    if (!instance) {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
+
+    // Display the object (WGPUInstance is a simple pointer, it may be
+    // copied around without worrying about its size).
+    std::cout << "WGPU instance: " << instance << std::endl;
+    
+    std::cout << "Hello Bear!" << std::endl;
+    return 0;
+}
